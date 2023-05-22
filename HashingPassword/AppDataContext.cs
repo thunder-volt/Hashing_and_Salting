e7f02d3c4c379445296ee9c86d1e5afdab4f2220
thunder-volt <ishu114407@gmail.com>
2023-05-22 22:27:09 +0530

    Add project files.

---
+++ HashingPassword/AppDataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashingPassword
+{
+    public class AppDataContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=App;Integrated Security=True;");
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ HashingPassword/Program.cs
@@ -0,0 +1,91 @@
+﻿using HashingPassword;
+using Microsoft.Identity.Client;
+using System.Security.Cryptography;
+using System.Text;
+using static System.Console;
+
+Prompt();
+
+void Prompt()
+{
+    Clear();
+    WriteLine("[R] Register [L] login");
+    
+    while (true)
+    {
+        var input = ReadLine().ToUpper()[0];
+        switch (input)
+        {
+            case 'R': Register(); break;
+            case 'L': LogIn(); break;
+            default:
+                break;
+        }
+    }
+}
+
+void LogIn()
+{
+    Clear();
+    WriteLine("################Login################");
+    WriteLine("User Name");
+    var name = ReadLine();
+    WriteLine("Password");
+    var password = ReadLine();
+    using AppDataContext context = new AppDataContext();
+    var userFound=context.Users.Any(u => u.Name == name);
+    if (userFound)
+    {
+        var loginUser=context.Users.FirstOrDefault(u => u.Name == name);
+
+        if (PasswordHash($"{password}{loginUser.Salt}") == loginUser.Password)
+        {
+            Clear();
+            ForegroundColor = ConsoleColor.Green;
+            WriteLine("Login Successful");
+            ReadLine();
+        }
+        else
+        {
+            Clear();
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine("Login Failed");
+            ReadLine();
+        }
+    }
+}
+
+void Register()
+{
+    Clear();
+    WriteLine("################Register################");
+    WriteLine("User Name");
+    var name = ReadLine();
+    WriteLine("Password");
+    var password = ReadLine();
+    using AppDataContext context = new AppDataContext();
+
+    var salt = DateTime.Now.ToString();
+    var HashedPassword = PasswordHash($"{password}{salt}");
+    context.Users.Add(new User() { Name = name, Password = HashedPassword, Salt = salt });
+    context.SaveChanges();
+    while (true)
+    {
+        Clear();
+        WriteLine("Registration Complete");
+        WriteLine("[B] Back");
+        if(ReadKey().Key == ConsoleKey.B)
+        {
+            Prompt();
+        }
+    }
+}
+
+string PasswordHash(string password)
+{
+    SHA256 hash= SHA256.Create();
+    var passwordBytes=Encoding.Default.GetBytes(password);
+    var hashedPassword=hash.ComputeHash(passwordBytes);
+    return Convert.ToHexString(hashedPassword);
+
+}
